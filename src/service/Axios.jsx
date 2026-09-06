706892ff070d9d92dadbd9c9aaf07abd1092d5ed
TanishqRathi12 <rathitanishq2022@gmail.com>
2026-09-07 00:37:35 +0530

    final commit -2

--- src/service/Axios.jsx
+++ src/service/Axios.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
-// import.meta.env.VITE_API_URL ||
-const API_BASE_URL =  "http://localhost:5000";
+
+const API_BASE_URL = import.meta.env.VITE_API_URL || "http://localhost:5000";
 
 //Axios instance with a base URL and default headers for making API requests to the backend server.
 const instance = axios.create({ 
