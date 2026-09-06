3956a74dabc0892a11e24bd9e16a3a30a17a0e8d
TanishqRathi12 <rathitanishq2022@gmail.com>
2026-09-06 20:26:57 +0530

    error stage-4

--- src/service/Axios.jsx
+++ src/service/Axios.jsx
@@ -1,11 +1,11 @@
 import axios from "axios";
 
 
-const API_BASE_URL = import.meta.env.VITE_API_URL || "http://localhost:5000/api";
+const API_BASE_URL = import.meta.env.VITE_API_URL || "http://localhost:5000";
 
 //Axios instance with a base URL and default headers for making API requests to the backend server.
 const instance = axios.create({ 
-  baseURL: `${API_BASE_URL}/api/employees`,
+  baseURL: `${API_BASE_URL}api/employees`,
   timeout: 10000,
   headers: {
     "Content-Type": "application/json",
